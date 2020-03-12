1d3be02c2aaba2de074a86bd3bcc6a7a429f9de4
Lonami Exo <totufals@hotmail.com>
2020-03-12 18:12:27 +0100

    Add basic source merging

--- server/rest.py
+++ server/rest.py
@@ -1,3 +1,6 @@
+import itertools
+import uuid
+
 from aiohttp import web
 
 from . import citehub
@@ -24,6 +27,41 @@ def adapt_academics_publication(data):
         'sources': ['ms-academics'],
     }
 
+def merge_data(*data):
+    assert len(data) != 0
+    return {
+        'id': f'merged-{uuid.uuid4()}',
+        'name': data[0]['name'],
+        'cites': next((d['cites'] for d in data if d['cites']), None),
+        'year': next((d['year'] for d in data if d['year']), None),
+        'authors': list(itertools.chain(*(d['authors'] for d in data))),
+        'publisher': next((d['publisher'] for d in data if d['publisher']), None),
+        'sources': list(itertools.chain(*(d['sources'] for d in data))),
+    }
+
+def merge_sorted_result(result):
+    # Merge may be automatic based on some similarity threshold,
+    # or interactive by asking the user to merge, in which case
+    # they choose what version to keep or even edit the details.
+    #
+    # For example, author names may vary a bit, or the publication
+    # title, but exact match should definitely be automatic.
+    #
+    # If we have the DOI, then it's clear when to merge.
+    #
+    # Ideally, when merging, we need the DOI of the cites.
+    # See also DBLP for articles (sadly, has no cite information.)
+    i = 0
+    limit = len(result) - 1
+    while i < limit:
+        a, b = result[i], result[i + 1]
+        if a['name'] == b['name']:
+            result[i] = merge_data(a, b)
+            result.pop(i + 1)
+            limit -= 1
+
+        i += 1
+
 async def get_publications(request):
     result = []
 
@@ -36,6 +74,9 @@ async def get_publications(request):
         '***REMOVED***'
     )
     result.extend(map(adapt_academics_publication, data['entities']))
+
+    result.sort(key=lambda x: x['name'])
+    merge_sorted_result(result)
 
     return web.json_response(result)
 
