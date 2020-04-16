503eebb55d7cc0450269b9576a268310023c6987
Lonami Exo <totufals@hotmail.com>
2020-04-16 20:55:18 +0200

    First attempt at a merge algorithm

--- server/datamodel.py
+++ server/datamodel.py
@@ -1,6 +1,18 @@
 from dataclasses import dataclass
 from enum import Enum
-from typing import List
+from typing import List, Generator
+import statistics
+import itertools
+
+
+def pairwise(iterable):
+    a, b = itertools.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+# Default threshold to consider two different data the same
+SIMILARITY_THRESHOLD = 0.9
 
 
 class Source(Enum):
@@ -95,3 +107,55 @@ class MergedPublication:
     left: Publication
     kind: MergeType
     right: Publication
+
+
+def merge_publications(
+        publications: List[Publication],
+        threshold: float = SIMILARITY_THRESHOLD,
+) -> Generator[MergedPublication]:
+    """
+    Merges publications from various sources into one.
+    """
+
+    # Work on a copy, we will be deleting items from the list as we go.
+    publications = publications[:]
+
+    similar_groups = []
+
+    while publications:
+        pub_i = publications.pop()
+        done = False
+
+        # Check if it fits in any of the previous groups
+        for group in similar_groups:
+            sim = statistics.mean(pub_i.similarity(pub) for pub in group)
+            if sim >= threshold:
+                group.append(pub_i)
+                done = True
+                break
+
+        if done:
+            continue
+
+        # If it doesn't fit in any of the previous groups try to pair it with
+        # another individual publication.
+        for j in range(len(publications)):
+            pub_j = publications[j]
+            sim = pub_i.similarity(pub_j)
+            if sim >= threshold:
+                similar_groups.append([pub_i, pub_j])
+                del publications[j]
+                done = True
+                break
+
+        if done:
+            continue
+
+        # If none of the other publications are similar to this one make a
+        # group with this publication alone.
+        similar_groups.append([pub_i])
+
+    # Yield the merged results with the similar groups
+    for group in similar_groups:
+        for left, right in pairwise(group):
+            yield MergedPublication(left=left, kind=MergeType.AUTOMATIC, right=right)
