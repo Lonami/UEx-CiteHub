867e34c0a30e05599f4d5c7ed38d1edafe25b71f
Lonami Exo <totufals@hotmail.com>
2020-07-25 20:29:35 +0200

    Allow sorting publications

--- client/src/Publications.svelte
+++ client/src/Publications.svelte
@@ -2,6 +2,42 @@
     import Publication from './Publication.svelte';
 
     import { get_publications } from './rest.js';
+
+    let sort_by = {key: null, rev: false};
+
+    // bit ugly because we want sorting to work with async; probably there is a better way
+    let pubs;
+    let publications = null;
+    async function load_publications() {
+        pubs = await get_publications();
+        set_sort(null);
+    }
+
+    function set_sort(key) {
+        if (sort_by.key === key) {
+            sort_by.rev = !sort_by.rev;
+        } else {
+            sort_by.key = key;
+            sort_by.rev = false;
+        }
+
+        if (sort_by.key === null) {
+            publications = pubs.publications;
+        } else {
+            let value = sort_by.rev ? -1 : 1;
+            let sorted = pubs.publications.slice();
+            sorted.sort(function(a, b) {
+                if (a[sort_by.key] < b[sort_by.key]) {
+                    return -value;
+                } else if (a[sort_by.key] > b[sort_by.key]) {
+                    return value;
+                } else {
+                    return 0;
+                }
+            });
+            publications = sorted;
+        }
+    }
 </script>
 
 <style>
@@ -14,30 +50,30 @@
         font-variant: small-caps;
         background-color: #eee;
         padding: 2px 4px;
+        cursor: pointer;
     }
 </style>
 
 <div class="publications">
-    {#await get_publications()}
+    {#await load_publications()}
         <p>Loading publications…</p>
-    {:then result}
+    {:then _}
         <h2>Metrics</h2>
         <ul>
-            <li>h-index: <strong>{result.h_index}</strong></li>
+            <li>h-index: <strong>{pubs.h_index}</strong></li>
         </ul>
         <table>
-            <!-- TODO Allow sorting -->
             <thead>
                 <tr>
-                    <th>Source</th>
+                    <th on:click={_ => set_sort(null)}>Source</th>
                     <!-- TODO would be nice to link to the website and see the entry on the corresponding source -->
-                    <th>Title</th>
-                    <th>Cited by</th>
+                    <th on:click={_ => set_sort("name")}>Title</th>
+                    <th on:click={_ => set_sort("cites")}>Cited by</th>
                     <!-- <th>Year</th> -->
                 </tr>
             </thead>
             <tbody>
-                {#each result.publications as publication (publication.id)}
+                {#each publications as publication}
                     <Publication {publication}/>
                 {/each}
             </tbody>
